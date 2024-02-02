dd3efdca2984f1d04b4184b6fb0f0596530e4097
KaguraiYoRoy <3363384229@qq.com>
2024-02-02 23:51:01 +0800

    add groups in members

--- mka.bat
+++ mka.bat
@@ -1,3 +1,3 @@
 @echo off
-sphinx-build .\source .\build
+C:\Users\kagur\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\sphinx-build .\source .\build
 pause
--- source/basic/Members.md
+++ source/basic/Members.md
@@ -1,1 +1,4 @@
 # 成员列表
+## 开发组 Developers
+## 管理组 Administrators
+## 运维组 Maintainers 
